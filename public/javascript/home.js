731a01d247135944e857a34401e8f71307410083
zlivehe55 <71079664+zlivehe55@users.noreply.github.com>
2024-02-20 19:35:05 -0500

    Update home.js

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -808,7 +808,7 @@ window.addEventListener('popstate', (event) => {
 
 
 // function([string1, string2],target id,[color1,color2])    
-consoleText(['Executiveconsults Creates.', 'Market.', 'Deploy '], 'text',['tomato','rebeccapurple','lightblue']);
+consoleText(['Executiveconsults.', 'Market.', 'Deploy '], 'text',['tomato','rebeccapurple','lightblue']);
 
 function consoleText(words, id, colors) {
 if (colors === undefined) colors = ['#fff'];
